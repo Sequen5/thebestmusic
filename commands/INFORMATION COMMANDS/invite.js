fdc34c05432731f06996ceeb6e7958f000418519
Sequen5 <52377112+Sequen5@users.noreply.github.com>
2021-04-10 11:39:55 +0000

    Add files via upload

---
+++ commands/INFORMATION COMMANDS/invite.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const config = require("../../config.json")
+module.exports = {
+    name: "invite",
+    aliases: ["add"],
+    category: "INFORMATION COMMANDS",
+    description: "Invite the Bot to your Server",
+    useage: "invite",
+    run: async (client, message, args) => {
+        let inviteembed = new Discord.MessageEmbed()
+            .setColor(config.colors.yes)
+            .setTitle("Invite, Sequen Music")
+            .setDescription("[INVITE ME NOW, thanks](https://discordapp.com/oauth2/authorize?client_id=660449158599016457&scope=bot&permissions=8)")
+            .setFooter("Sequen Music", client.user.displayAvatarURL())
+
+        message.reply(inviteembed);
+    }
+}
+
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention Him / Milrato Development, when using this Code!
+ * @INFO
+ */
